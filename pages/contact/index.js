db0eb219102fc847ff8f3b7b57a4580615e8da64
Patrizio Tardiolo Bonifazi <p.tardiolobonifazi@vivasoft.it>
2024-11-19 17:13:05 +0100

    Era nel repository di Beatrice Bottini https://github.com/Fenice78/Template_Vivasoft Ora in: https://github.com/Patrita63/Template_Vivasoft-main
    // TODO PATRIZIO
        // Gestire il ritorno dell'invio della mail
        // Attualmente non la manda

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,5 +1,5 @@
 // components
-import Circles from '/components/Circles';
+// import Circles from '/components/Circles';
 
 // icons
 import { BsArrowRight } from 'react-icons/bs';
@@ -10,13 +10,25 @@ import { motion } from 'framer-motion';
 // variants
 import { fadeIn } from '../../variants';
 
+// 
+import { SendMailResend } from '../api/send'
 // https://resend.com/nextjs
 import { useState, useEffect } from 'react';
-// import { EmailTemplate } from '@/components/email-template';
 // import { Resend } from 'resend';
+// import { EmailTemplate } from '@/components/email-template';
+// import { NextRequest, NextResponse } from 'next/server';
+
+// https://nextjs.org/docs/pages/api-reference/functions/use-router
+import { useRouter } from 'next/router';
+// import { NextResponse, NextRequest } from 'next/server'
+// https://nextjs.org/docs/pages/building-your-application/routing/redirecting
 
 
 const Contact = () => {
+
+  // To navigate to another page
+  const router = useRouter();
+
   let [fullname, setFullname] = useState('');
   let [mailAddress, setMailAddress] = useState('');
   let [mailSubject, setMailSubject] = useState('');
@@ -53,7 +65,20 @@ const Contact = () => {
     setMailBody(event.target.value);
   }
 
-  const SendMail = (event) => {
+  const SendMail = async (event) => {
+   // https://resend.com/docs/knowledge-base/how-do-i-fix-cors-issues
+/*     Access to XMLHttpRequest at 'https://api.resend.com/emails'
+    from origin 'http://localhost:3000' has been blocked by CORS policy:
+    Response to preflight request doesn't pass access control check:
+    No 'Access-Control-Allow-Origin' header is present on the requested resource. */
+
+    const RESEND_API_KEY = "re_EPLAJcV9_6RyTtaemEfcSnVBnUinGkNV1";
+    // from: 'onboarding@resend.dev'
+    // https://resend.com/onboarding
+
+    // Access to fetch at 'https://api.resend.com/emails' from origin 'http://localhost:3000' has been blocked by CORS policy: 
+    const RESEND_MAIL_FROM = "onboarding@resend.dev";
+    const RESEND_MAIL_TO = "p.tardiolobonifazi@vivasoft.it";
     // alert('SendMail');
     event.preventDefault();
     console.log(event.target);
@@ -62,9 +87,68 @@ const Contact = () => {
     // alert('Nominativo = ' + dataMailFullName);
     const dataMailAddress = mailAddress; //event.target.form[1].value; //event.target.email.value;
     const dataMailSubject = mailSubject; //event.target.form[2].value; //event.target.subject.value;
-    const dataMailBody = mailBody; // event.target.form[3].value; //event.target.body.value;
-    alert(`${dataMailFullName}  send an email to ${dataMailAddress} - Subject: ${dataMailSubject} - Body: ${dataMailBody}`);
+
+    const body =  `${dataMailFullName}  sent an email from ${dataMailAddress} - Subject: ${dataMailSubject} - Body: ${mailBody}`
+    // alert(body);
+
+    const dataMailBody = body; // event.target.form[3].value; //event.target.body.value;
+
+    SendMailResend(mailSubject, dataMailBody);
+
+    // TODO PATRIZIO
+    // Gestire il ritorno dell'invio della mail
+    // Attualmente non la manda
+    
+    // Redirect to home page 
+    router.push("/");
+
+
+    // https://resend.com/nextjs
+    // const resend = new Resend(process.env.RESEND_API_KEY);
+
+   // https://resend.com/docs/api-reference/emails/send-email
+   // const resend = new Resend(RESEND_API_KEY);
+
+    /* try {
+      const response = await fetch('https://api.resend.com/emails/api/send', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+              'Authorization': 'Bearer re_EPLAJcV9_6RyTtaemEfcSnVBnUinGkNV1'
+          },
+          from: RESEND_MAIL_FROM,
+          to: RESEND_MAIL_TO,
+          subject: mailSubject,
+          html: dataMailBody
+          // body: JSON.stringify(values)
+      });
+
+      if (response.ok) {
+          console.log('Email sent successfully!');
+      } else {
+          const errorDetails = await response.json();
+          console.error('Error sending email:', errorDetails.message);
+      }
+    } catch (error) {
+      console.error('There was a problem sending the email:', error);
+    } */
+
+    /* const response = await resend.emails.send({
+      from: RESEND_MAIL_FROM,
+      to: RESEND_MAIL_TO,
+      subject: mailSubject,
+      html: dataMailBody
+    });
+
+    console.log(response); */
+
+    /* if (error) {
+      return res.status(400).json(error);
+    }
+  
+    return res.status(200).json(data); */
   }
+
 
   return (
     <div className='h-full bg-primary'>
