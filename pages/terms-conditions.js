dd098414f2d86d9cedffbedcb2965f229e35dd3b
Patrizio Tardiolo Bonifazi <p.tardiolobonifazi@vivasoft.it>
2024-11-28 20:45:58 +0100

    TermsConditions

---
+++ pages/terms-conditions.js
@@ -0,0 +1,14 @@
+
+
+const TermsConditions = () => {
+
+    return ( 
+        <>
+            La presente Politica su Termini e Condizioni è stata aggiornata al 28 novembre 2024
+
+            Termini e Condizioni
+        </>
+    )
+}
+
+export default TermsConditions;
